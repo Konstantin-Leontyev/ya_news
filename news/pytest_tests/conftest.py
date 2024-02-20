34851ac371cbc9754dfdce30fda695694991873f
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-02-20 12:57:01 +0400

    Add pytest test_routes

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,69 @@
+# conftest.py
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from news.models import News, Comment
+
+
+@pytest.fixture
+# Используем встроенную фикстуру для модели пользователей django_user_model.
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):  # Вызываем фикстуру автора.
+    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+    return client
+
+
+@pytest.fixture
+def news(author):
+    news = News.objects.create(  # Создаём объект заметки.
+        title='Заголовок',
+        text='Текст',
+    )
+    return news
+
+
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(  # Создаём объект заметки.
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+# Фикстура запрашивает другую фикстуру создания заметки.
+def news_id_for_args(news):
+    # И возвращает кортеж, который содержит slug заметки.
+    # На то, что это кортеж, указывает запятая в конце выражения.
+    return (news.id,)
+
+
+@pytest.fixture
+# Фикстура запрашивает другую фикстуру создания заметки.
+def comment_id_for_args(comment):
+    # И возвращает кортеж, который содержит slug заметки.
+    # На то, что это кортеж, указывает запятая в конце выражения.
+    return (comment.id,)
