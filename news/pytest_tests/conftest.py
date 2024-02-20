0f89592899f652cc271f44145bd8fb8f90d1379c
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-02-20 17:01:27 +0400

    Add pytest test_contest

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -1,8 +1,12 @@
 # conftest.py
+from datetime import datetime, timedelta
+
 import pytest
+from django.conf import settings
 
 # Импортируем класс клиента.
 from django.test.client import Client
+from django.urls import reverse
 
 # Импортируем модель заметки, чтобы создать экземпляр.
 from news.models import News, Comment
@@ -37,20 +41,53 @@ def not_author_client(not_author):
 @pytest.fixture
 def news(author):
     news = News.objects.create(  # Создаём объект заметки.
-        title='Заголовок',
-        text='Текст',
+        title='Тестовая новость',
+        text='Просто текст.',
     )
     return news
 
 
 @pytest.fixture
+def news_factory(author, comment):
+    today = datetime.today()
+    all_news = [
+        News(
+            title=f'Тестовая новость {index}',
+            text='Просто текст.',
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        # Для каждой новости уменьшаем дату на index дней от today,
+        # где index - счётчик цикла.
+    ]
+    News.objects.bulk_create(all_news)
+
+
+@pytest.fixture
 def comment(author, news):
-    comment = Comment.objects.create(  # Создаём объект заметки.
+    comment = Comment.objects.create(  # Создаём объект новости.
         news=news,
         author=author,
         text='Текст комментария'
     )
     return comment
+
+
+@pytest.fixture
+def comment_factory(author, news):
+    today = datetime.today()
+    all_comments = [
+        Comment(
+            news=news,
+            author=author,
+            text='Текст комментария',
+            created=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        # Для каждой новости уменьшаем дату на index дней от today,
+        # где index - счётчик цикла.
+    ]
+    Comment.objects.bulk_create(all_comments)
 
 
 @pytest.fixture
@@ -67,3 +104,9 @@ def comment_id_for_args(comment):
     # И возвращает кортеж, который содержит slug заметки.
     # На то, что это кортеж, указывает запятая в конце выражения.
     return (comment.id,)
+
+
+@pytest.fixture
+def detail_url(news):
+    return reverse('news:detail', args=(news.id,))
+
